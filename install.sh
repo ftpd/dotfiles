e876823370f35862a1be39d8ac0e34fd6077b59f
Bartek Stalewski <ftpd@insomniac.pl>
2013-08-19 13:53:20 +0200

    New dircolors.

--- install.sh
+++ install.sh
@@ -3,38 +3,38 @@
 # cleaning
 rm -rf ~/.dotfiles ~/.oh-my-zsh ~/.zshrc ~/.vim ~/.vimrc ~/.gvimrc ~/.gitignore ~/.tmux.conf
 
-# zsh
+# repos
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+if [ `hostname` = "tuonela" ] && [ `whoami` = "bartek.stalewski" ]; then
+  path="git@bitbucket.org:ftpd/dotfiles.git"
+else
+  path="https://bitbucket.org/ftpd/dotfiles.git"
+fi
+git clone $path ~/.dotfiles
+cd ~/.dotfiles
+git submodule init
+git submodule update
+cd vim/bundle/powerline
+git checkout -b develop origin/develop
+
+# files
+
+## zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 sed -i -e 's/robbyrussell/nicoulaj/' ~/.zshrc
 sed -i -e 's/# CASE_SENSITIVE/CASE_SENSITIVE/' ~/.zshrc
 case `uname` in
-  Linux) echo 'eval $( dircolors -b ~/.dotfiles/dircolors ) ' >> ~/.zshrc ;;
-  Darwin) echo 'eval $( gdircolors -b ~/.dotfiles/dircolors ) ' >> ~/.zshrc ;;
+  Linux) echo 'eval $( dircolors -b ~/.dotfiles/dircolors/dircolors.ansi-universal ) ' >> ~/.zshrc ;;
+  Darwin) echo 'eval $( gdircolors -b ~/.dotfiles/dircolors/dircolors.ansi-universal ) ' >> ~/.zshrc ;;
 esac
 echo "zstyle ':vcs_info:*' enable hg bzr git svn" >> ~/.zshrc
 echo "[ -e ~/.zshrc.local ] && source ~/.zshrc.local" >> ~/.zshrc
-
-# ftpd/doftiles
-path="https://bitbucket.org/ftpd/dotfiles.git"
-
-if [ `hostname` = "tuonela" ] && [ `whoami` = "bartek.stalewski" ]; then
-	path="git@bitbucket.org:ftpd/dotfiles.git"
-fi
-git clone $path ~/.dotfiles
 
 # vim
 ln -sf ~/.dotfiles/vim ~/.vim
 ln -sf ~/.vim/rcfiles/vimrc ~/.vimrc
 ln -sf ~/.vim/rcfiles/gvimrc ~/.gvimrc
 rm -rf ~/.vim/bundle/*
-#git submodule add git://github.com/Lokaltog/vim-powerline.git vim/bundle/powerline
-#git submodule add git://github.com/scrooloose/nerdtree.git vim/bundle/nerdtree
-cd ~/.dotfiles
-git submodule init
-git submodule update
-cd vim/bundle/powerline
-git checkout -b develop origin/develop
 
 # .gitignore
 ln -sf ~/.dotfiles/gitignore ~/.gitignore
