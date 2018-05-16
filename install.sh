afe00c4c0948f9d32990c4cc345811c12d3f0853
Bartek Stalewski <ftpd@insomniac.pl>
2018-05-16 12:33:05 +0200

    Switch to neovim

--- install.sh
+++ install.sh
@@ -9,10 +9,13 @@ git clone https://bitbucket.org/ftpd/dotfiles.git ~/.dotfiles
 
 ## vim
 # cleaning
+rm -rf ~/.vim*
 rm -rf ~/.config/nvim
 # installing
 mkdir -p ~/.config
 ln -sf ~/.dotfiles/vim ~/.config/nvim
+ln -sf ~/.dotfiles/vim ~/.vim
+ln -sf ~/.dotfiles/vim/init.vim ~/.vimrc
 
 ## .gitignore
 # cleaning
