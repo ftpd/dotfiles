49c58da9f502f75822d9dff660953c8e14985142
Bartek Stalewski <ftpd@insomniac.pl>
2020-01-10 17:57:57 +0100

    k8s support

---
+++ zsh/k8s.zsh
@@ -0,0 +1,19 @@
+slimline::section::k8s_context::precmd() {
+  unset slimline_section_k8s_context_output
+}
+
+slimline::section::k8s_context::async_task() {
+  kubectl config current-context
+  # gsed -nr '/current-context/s/(.*) (.*)/\2/p' ~/.kube/config
+}
+
+slimline::section::k8s_context::async_task_complete() {
+  slimline_section_k8s_context_output=$2
+}
+
+slimline::section::k8s_context::render() {
+  [[ -z "${slimline_section_k8s_context_output}" ]] && return
+
+  slimline::utils::expand "k8s_context" "[k8s:%F{blue}|output|%f]" \
+      "output" "${slimline_section_k8s_context_output}" \
+}
