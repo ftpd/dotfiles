f993421ac98f7f9d7e5d93111761ab11580e84a6
Bartek Stalewski <ftpd@insomniac.pl>
2020-01-10 23:32:07 +0100

    Cleaning.

--- legacy/agnoster.zsh
+++ legacy/agnoster.zsh
@@ -1,12 +1,15 @@
-git clone https://github.com/popstas/zsh-command-time.git ~/.oh-my-zsh/custom/plugins/command-time
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+export ZSH=$HOME/.oh-my-zsh
 ZSH_THEME="agnoster"
-plugins=(git kube-ps1 command-time zsh-autosuggestions zsh-syntax-highlighting)
+CASE_SENSITIVE="true"
+ENABLE_CORRECTION="true"
+plugins=(git kube-ps1 timer zsh-autosuggestions zsh-syntax-highlighting)
+source $ZSH/oh-my-zsh.sh
 export DEFAULT_USER=$LOGNAME
 source ~/.dotfiles/zsh/abbr_pwd
 prompt_dir() {
   prompt_segment blue $CURRENT_FG $(felix_pwd_abbr)
 }
 PROMPT=$PROMPT'$(kube_ps1) '
-ZSH_COMMAND_TIME_MIN_SECONDS=2
-ZSH_COMMAND_TIME_MSG="(took $fg[yellow]%s$fg[default])"
-[ -e ~/.zshrc.local ] && source ~/.zshrc.local
