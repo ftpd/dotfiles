08aadb1b4a700a9af27df8639b48d8bd1e433167
Bartek Stalewski <ftpd@insomniac.pl>
2020-10-22 16:44:38 +0200

    Neovim + XDG setup.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 rm -rf ~/.bash* ~/.profile
 rm -rf ~/.dotfiles
 rm -rf ~/.config/fd ~/.config/git ~/.config/nvim ~/.config/tmux ~/.config/zsh ~/.zshrc
-rm -rf ~/.fdignore ~/.fzf.bash ~/.fzf.zsh ~/.gitconfig ~/.gitignore ~/.history ~/.tmux.conf ~/.zcomp* ~/.zsh*
+rm -rf ~/.fdignore ~/.fzf.bash ~/.fzf.zsh ~/.gitconfig ~/.gitignore ~/.history ~/.tmux.conf ~/.vim* ~/.zcomp* ~/.zsh*
 
 # repo + preparations
 git clone https://github.com/ftpd/dotfiles.git ~/.dotfiles
@@ -22,7 +22,8 @@ ln -sf ~/.dotfiles/fd ~/.config/
 ln -sf ~/.dotfiles/tmux ~/.config/
 
 ## zsh
-mkdir -p ~/.cache/zsh ~/.config/zsh/caches ~/.config/zsh/plugins ~/.config/zsh/themes
+echo "skip_global_compinit=1" > ~/.zshenv
+mkdir -p ~/.cache/zsh ~/.config/zsh/plugins ~/.config/zsh/themes
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.config/zsh/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/plugins/zsh-syntax-highlighting
 git clone https://github.com/romkatv/powerlevel10k.git ~/.config/zsh/themes/powerlevel10k
