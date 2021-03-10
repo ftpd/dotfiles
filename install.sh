261e3d69763af2e5ec455a9ec49ff3464b21114d
Bartek Stalewski <ftpd@insomniac.pl>
2021-03-10 09:42:49 +0100

    More compliant with XDG standards

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # cleaning
 rm -rf ~/.bash* ~/.profile
 rm -rf ~/.dotfiles
-rm -rf ~/.cache ~/.config/_cache ~/.config/fd ~/.config/git/ignore ~/.config/nvim ~/.config/tmux ~/.config/vim ~/.config/zsh ~/.local ~/.zshenv
+rm -rf ~/.cache ~/.config/_cache ~/.config/fd ~/.config/git/ignore ~/.config/nvim ~/.config/tmux ~/.config/vim ~/.config/zsh ~/.zshenv
 rm -rf ~/.fdignore ~/.fzf.bash ~/.fzf.zsh ~/.gitconfig ~/.gitignore ~/.history ~/.tmux.conf ~/.vim* ~/.zcomp* ~/.zsh*
 
 # repo + preparations
@@ -19,14 +19,16 @@ ln -sf ~/.dotfiles/git/ignore ~/.config/git/ignore
 
 ## fdignore
 ln -sf ~/.dotfiles/fd ~/.config/
+touch ~/.config/local/fd
 
 ## tmux
 ln -sf ~/.dotfiles/tmux ~/.config/
 
 ## zsh
 echo "skip_global_compinit=1" > ~/.zshenv
-echo "export XDG_DATA_HOME=~/.config/_cache" > ~/.zshenv
-echo "export XDG_CACHE_HOME=~/.config/_cache" >> ~/.zshenv
+echo "export XDG_DATA_HOME=~/.cache" > ~/.zshenv
+echo "export XDG_CACHE_HOME=~/.cache" >> ~/.zshenv
 echo "export XDG_CONFIG_HOME=~/.config" >> ~/.zshenv
 echo "export ZDOTDIR=~/.config/zsh" >> ~/.zshenv
 ln -sf ~/.dotfiles/zsh ~/.config/
+mkdir -p "$XDG_DATA_HOME/zsh"
